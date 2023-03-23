042352914213934e7b06035576f685d2808c2545
patrik <patrik.youssef@arbetsformedlingen.se>
2023-03-23 16:26:42 +0100

    Adds navigation to main title

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,13 @@
 import "./Header.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header__container container pt-3">
       <div className="row">
         <div className="col-10">
-          <h1>tayo</h1>
+          <Link className="header__main-link" to="/">
+            <h1>Tayo</h1>
+          </Link>
         </div>
         <nav className="col-2">
           <ul className="header__navigation-links">
