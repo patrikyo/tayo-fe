2656e6909f2478f8314c390b0fce4017003874a7
patrik <patrik.youssef@arbetsformedlingen.se>
2023-03-25 13:37:53 +0100

    Adds responsive design main paige

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,21 +1,23 @@
 import "./Header.css";
 import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faElevator } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
   return (
-    <div className="header__container container pt-3">
-      <div className="row">
-        <div className="col-10">
-          <Link className="header__main-link" to="/">
-            <h1>Tayo</h1>
-          </Link>
+    <div className="header__container pt-3">
+      <Link className="header__main-link" to="/">
+        <div className="header_main-container">
+          <h1>Tayo</h1>
+          <FontAwesomeIcon className="header_icon" icon={faElevator} />
         </div>
-        <nav className="col-2">
-          <ul className="header__navigation-links">
-            <li>{<NavLink to="/">Hem</NavLink>}</li>
-            <li>{<NavLink to="/contact">Kontakta</NavLink>}</li>
-          </ul>
-        </nav>
-      </div>
+      </Link>
+      <nav>
+        <ul className="header__navigation-links">
+          <li>{<NavLink to="/">Hem</NavLink>}</li>
+          <li>{<NavLink to="/contact">Kontakta</NavLink>}</li>
+        </ul>
+      </nav>
     </div>
   );
 }
