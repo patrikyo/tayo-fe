e7d6fca158953b19e3d64a8ab1971140f0432b7c
patrik <patrik.youssef@arbetsformedlingen.se>
2023-03-25 11:05:52 +0100

    Adds validation and verification msg

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = function () {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [sent, setSent] = useState(false);
 
   const sendMessage = (e) => {
+    setLoading(true);
     e.preventDefault();
     const contactForm = { name, email, message };
     fetch("https://tayo-api-service.onrender.com/api/contact", {
@@ -15,7 +20,21 @@ const Contact = function () {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(contactForm),
-    });
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          console.info(res, "Framgånsrikt");
+          setLoading(false);
+          setSent(true);
+        } else {
+          console.info("misslyckades");
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.info("misslyckades");
+        setLoading(false);
+      });
   };
 
   return (
@@ -29,6 +48,7 @@ const Contact = function () {
           <legend></legend>
           <label htmlFor="name">Namn</label>
           <input
+            required
             className="contact__control"
             id="name"
             type="text"
@@ -37,6 +57,7 @@ const Contact = function () {
           />
           <label htmlFor="email">Email</label>
           <input
+            required
             className="contact__control"
             id="email"
             type="email"
@@ -50,7 +71,26 @@ const Contact = function () {
             onChange={(e) => setMessage(e.target.value)}
           ></textarea>
 
-          <input className="contact__submit" type="submit" value="Skicka" />
+          {!loading && !sent && (
+            <input className="contact__submit" type="submit" value="Skicka" />
+          )}
+
+          {sent && (
+            <div className="contact__verification">
+              <FontAwesomeIcon
+                className="contact__check-icon"
+                icon={faCheckCircle}
+              />
+              <p class="mt-3 text-center">
+                Tack för kontaktuppgifterna, vi återkommer så snabbt vi kan
+              </p>
+            </div>
+          )}
+          {loading && (
+            <div className="contact__spinner-container">
+              <div class=" spinner-border text-primary" role="status"></div>
+            </div>
+          )}
         </fieldset>
       </form>
     </div>
