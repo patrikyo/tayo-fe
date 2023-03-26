22e7766e5d2411376b632f368e71cc9ac81260c7
patrik <patrik.youssef@arbetsformedlingen.se>
2023-03-26 16:51:50 +0200

    Adds navigation btn and responsive design

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,24 +2,49 @@ import "./Header.css";
 import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faElevator } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Header() {
+  const [navigation, setNavigation] = useState(false);
+
   return (
-    <div className="header__container pt-3">
-      <Link className="header__main-link" to="/">
-        <div className="header_main-container">
+    <div className="header pt-3">
+      {/** title */}
+      <Link id="header__home-link" to="/">
+        <div className="header__title-container">
           <h1>Tayo</h1>
-          <FontAwesomeIcon className="header_icon" icon={faElevator} />
+          <FontAwesomeIcon className="header__icon" icon={faElevator} />
         </div>
       </Link>
-      <nav>
-        <ul className="header__navigation-links">
-          <li>{<NavLink to="/">Hem</NavLink>}</li>
-          <li>{<NavLink to="/contact">Kontakta</NavLink>}</li>
-        </ul>
-      </nav>
+
+      {/** navigation links */}
+      <div>
+        {/** Btn only visible up to 540px */}
+        <button
+          className="header__navigation-btn"
+          onClick={() => setNavigation(!navigation)}
+        >
+          <FontAwesomeIcon id="header__bars-icon" icon={faBars} />
+        </button>
+
+        <nav>
+          <ul
+            className={`header__navigation-links  ${
+              navigation === true
+                ? "header__navigation-links--visible"
+                : "header__navigation-links--hide"
+            }`}
+          >
+            <li>{<NavLink to="/">Hem</NavLink>}</li>
+            <li>{<NavLink to="/contact">Kontakta</NavLink>}</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
 
 export default Header;
+
+// {
