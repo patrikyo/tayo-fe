31bcc43a8655ca21a7741320dc477d6b66a97eab
patrik <patrik.youssef@arbetsformedlingen.se>
2023-04-15 16:34:06 +0200

    Centers  header

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,10 +2,8 @@ import "./Hero.css";
 function Hero() {
   return (
     <div className="hero__container">
-      <div className="container">
-        <div className="hero__text-container">
-          <h2 className="hero__header">Rulltrappor & Hissar</h2>
-        </div>
+      <div className="hero__text-container">
+        <h2 className="hero__header">Rulltrappor & Hissar</h2>
       </div>
     </div>
   );
