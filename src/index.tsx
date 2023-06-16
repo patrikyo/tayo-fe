1ca4ad3c74f8fe6ff8b7e248edb27a08f6db6d94
patrik <patrik.youssef@arbetsformedlingen.se>
2023-06-16 13:56:09 +0200

    Adds redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
 import "./index.scss";
-
+import { Provider } from "react-redux";
+import store from "./store"
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+     <App />
+    </Provider>
   </React.StrictMode>
 );
