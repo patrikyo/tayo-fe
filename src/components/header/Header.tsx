248d517450f2585979ad42deff5a074efd54b003
patrik <patrik.youssef@arbetsformedlingen.se>
2023-06-19 10:26:02 +0200

    Replaces useState with redux state managment

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,34 +3,36 @@ import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faElevator } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { setNav } from '../../features/appSlice';
 
 function Header(): JSX.Element {
-  const [nav, setNav]:  [boolean, Function] = useState<boolean>(false);
+  const app = useSelector((state: any) => state);
+  const dispatch = useDispatch();
 
   return (
     <div className={`header`}>
-      {/** title starts*/}
+      {/* title starts */}
       <Link id="header__home-link" to="/">
         <div className="header__title-container">
           <h1>Tayo</h1>
           <FontAwesomeIcon className="header__icon--title" icon={faElevator} />
         </div>
       </Link>
-      {/** title ends*/}
+      {/* title ends */}
 
-      {/** navigation links start */}
+      {/* navigation links start */}
       <div className="header__nav-container">
-        {/** Btn only visible up to 540px  starts */}
-        <button className="header__nav-toggle" onClick={() => setNav(!nav)}>
+        {/* Btn only visible up to 540px starts */}
+        <button className="header__nav-toggle" onClick={() => dispatch(setNav())}>
           <FontAwesomeIcon id="header__icon--bars" icon={faBars} />
         </button>
-        {/** Btn only visible up to 540px  ends */}
+        {/* Btn only visible up to 540px ends */}
 
         <nav>
           <ul
             className={`header__nav-list  ${
-              nav === true
+              app.nav === true
                 ? "header__nav-list--visible"
                 : "header__nav-list--hide"
             }`}
@@ -40,11 +42,8 @@ function Header(): JSX.Element {
           </ul>
         </nav>
       </div>
-      {/** navigation links ends */}
-    </div>
+      </div>
   );
 }
 
 export default Header;
-
-// {
